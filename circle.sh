1646867203de59e7ec62d4e20a681672242ab3cf
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-06-05 14:25:53 +0700

    Use Clang

--- circle.sh
+++ circle.sh
@@ -82,8 +82,8 @@ makekernel() {
     # Clean any old AnyKernel
     rm -rf ${ANYKERNEL}
     git clone https://github.com/Reinazhard/AnyKernel3 -b master anykernel3
-    export CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-linux-gnu-"
-    export CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-maestro-linux-gnueabi-"
+    #export CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-linux-gnu-"
+    #export CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-maestro-linux-gnueabi-"
     kernelstringfix
     export PATH="${KERNELDIR}/clang/bin:$PATH"
     make O=out ARCH=arm64 ${DEFCONFIG}
